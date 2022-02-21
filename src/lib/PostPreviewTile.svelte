f120973d2674376da3ad7d53538f1d759742e4b1
Scott Whittaker <scowhitt@gmail.com>
2022-02-21 20:57:02 +0000

    [feature] adds recent posts to home page

---
+++ src/lib/PostPreviewTile.svelte
@@ -0,0 +1,39 @@
+<script>
+    import PostDateTime from '$lib/PostDateTime.svelte';
+
+    export let post = {};
+    const {date, title, slug} = post;
+</script>
+
+<article>
+    <a href={`/posts/${slug}`}><h3 class="title">{title}</h3></a>
+    <PostDateTime {date} />
+</article>
+
+<style>
+    article {
+        padding: 1rem;
+        border-bottom: 1px solid var(--grey-light);
+    }
+
+    article h3 {
+        margin: 0;
+        color: inherit;
+    }
+
+
+    a {
+        text-decoration: none;
+        color: inherit;
+
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+
+    @media (min-width: 640px) {
+        article {
+            border: 1px solid var(--grey-light);
+        }
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,62 @@
+<script context="module">
+    export async function load() {
+        const posts = import.meta.globEager('./posts/*.md');
+        const postsList = Object.values(posts);
+        const postsMeta = postsList.map(post => post.metadata);
+        const sortedPosts = postsMeta.sort((a, b) => new Date(b.date) - new Date(a.date));
+        const recentPosts = sortedPosts.slice(0, 6);
+        return {
+            props: {
+                posts: recentPosts
+            }
+        };
+    }
+</script>
+
+<script>
+    import PostPreviewTile from '$lib/PostPreviewTile.svelte';
+
+    export let posts;
+</script>
+
 <svelte:head>
     <title>Scott Whittaker</title>
-    <meta name="description" content="Just another Sveltekit stater blog">
+    <meta name="description" content="Just dumping stuff of my brain occasionally.">
 </svelte:head>
 
-<h2>Home</h2>
+
+
+<div class="g-full-bleed">
+<div class="g-wrapper">
+    <h2>Recent posts</h2>
+    <div class="wrapper">
+{#each posts as post}
+    <PostPreviewTile post={post} />
+{/each}
+    </div>
+</div>
+</div>
+
+<style>
+    .g-full-bleed {
+        padding: 4rem 0;
+        background: var(--grey-lightest);
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        gap: 16px;
+        margin-top: 2rem;
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    @media (min-width: 640px) {
+        .wrapper {
+            grid-template-columns: fit-content(8ch) fit-content(8ch) 1fr;
+        }
+    }
+</style>
