05caa644250f85381c09f41ed3edda8a38a0bcd5
Scott Whittaker <scowhitt@gmail.com>
2022-03-07 16:38:08 +0000

    adds error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    /** @type {import('@sveltejs/kit').ErrorLoad} */
+    export function load({ error, status }) {
+        return {
+            props: {
+                message: error.message,
+                status
+            }
+        };
+    }
+</script>
+
+<script>
+    export let message;
+    export let status;
+</script>
+
+<h2>{status}</h2>
+<p>{message}</p>
